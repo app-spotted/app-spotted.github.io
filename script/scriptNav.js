988c200db9cd6f0bf3f170a93567fa7d9303e5c3
Erik Vicenzotto <66376109+Vicepredator11@users.noreply.github.com>
2022-03-21 22:54:37 +0100

    Fixed mobile NavBar prev Not opening

--- script/scriptNav.js
+++ script/scriptNav.js
@@ -1,14 +1,14 @@
 const navbar = document.getElementById("navigation");           //Navigation bar
 const navigationToggle = document.getElementById("toggle");     //Button to toggle the navigation bar
 
+//document.onload(setNavToggle());
 setNavToggle();
 
 // on the click of the nav toggle button i activate the showing nav
 function setNavToggle(){
-    navigationToggle.addEventListener('click', showNav);
-    function showNav(){
+    navigationToggle.addEventListener('click', function(){
         const visibility = navbar.getAttribute('data-visible');
         if(visibility === "false") navbar.setAttribute('data-visible', true);
         else navbar.setAttribute('data-visible', false);
-    }
+    });
 }
