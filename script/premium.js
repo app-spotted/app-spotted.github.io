f14b04f7e6059dd067a814948d6e199d1dcc944a
Erik Vicenzotto <erik.vicenzotto@gmail.com>
2022-04-12 14:55:32 +0200

    Added event on Payment form submit

--- script/premium.js
+++ script/premium.js
@@ -34,7 +34,7 @@ fetch("http://192.168.78.189:5000/config")
             : ""
         }>
           <div class="name">${prod.name}</div>
-          <div class="pricing">${price.unit_amount / 100} / ${
+          <div class="pricing">${price.unit_amount / 100} € / ${
           price.recurringInterval
         }</div>
           <hr>
@@ -112,3 +112,30 @@ const loadPaymentForm = () => {
     document.querySelector("#payButton").style.display = 'block';
   });
 };
+
+
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const {error} = await stripe.confirmPayment({
+    //`Elements` instance that was used to create the Payment Element
+    elements,
+    confirmParams: {
+      return_url: "https://example.com/order/123/complete",
+    }
+  });
+
+  if (error) {
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Show error to your customer (for example, payment
+    // details incomplete)
+    const messageContainer = document.querySelector('#error-message');
+    messageContainer.textContent = error.message;
+  } else {
+    // Your customer will be redirected to your `return_url`. For some payment
+    // methods like iDEAL, your customer will be redirected to an intermediate
+    // site first to authorize the payment, then redirected to the `return_url`.
+  }
+});
