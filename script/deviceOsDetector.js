cf3720c0bbf5f05d0134ff01150a3183ed7ea1ff
VTI-Software <erik.vicenzotto@gmail.com>
2025-06-03 16:47:32 +0200

    [FIX] Changed iPhone detection in generic Apple products detection.
    Since IOS 13 Safari doesn't change UA between mobiles and computers so we need to join the 2 categories in the "Apple" one.

--- script/deviceOsDetector.js
+++ script/deviceOsDetector.js
@@ -2,14 +2,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const select = document.getElementById("deviceOs");
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    //console.log('UserAgent: '+ userAgent);
+    console.log('UserAgent: '+ userAgent);
     let deviceType = "other";
 
     // Using RegEx to find 'android' or 'iphone' on the userAgent
     if (/android/i.test(userAgent)) {
     deviceType = "android";
-    } else if (/iPhone|iPad|iPod/i.test(userAgent) || (/Macintosh/i.test(userAgent) && /Mobile/i.test(userAgent))) {
-        deviceType = "ios";
+    } else if (/iPhone|iPad|iPod|Macintosh/i.test(userAgent)) {
+        deviceType = "apple";
     }
 
     // Setting select value accordingly
